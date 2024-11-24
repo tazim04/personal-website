c1def6bddf05effd2a1d9c58bf4dd0f1771eb0d3
tazim04 <35643104+tazim04@users.noreply.github.com>
2024-11-23 20:02:09 -0500

    changed html title

--- personal-website/src/components/NavBar.tsx
+++ personal-website/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Menu } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
